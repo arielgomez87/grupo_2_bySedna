ee26840531c546fca8b7a5a6dfc70218f5b80fb4
Nicoloman <nicolomanto@gmail.com>
2022-08-26 09:17:00 -0300

    install de componentes y routing

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, ()=>{console.log('Server Running on port 3000')});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//Routing
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
---
+++ controllers/indexController.js
@@ -0,0 +1,6 @@
+const indexController = {
+    home: (req,res)=>
+        res.render('home')
+}
+
+module.exports = indexController;
