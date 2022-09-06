786e327b0bcdbf77a2cc9abaa166f9061a0ccb5b
Nicoloman <nicolomanto@gmail.com>
2022-09-05 21:00:21 -0300

    #

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const indexController = {
     home: (req,res)=>
-        res.render('home'),
+        res.render('home', {products}),
     login: (req, res)=>
         res.render('login'),
     register: (req, res)=>
