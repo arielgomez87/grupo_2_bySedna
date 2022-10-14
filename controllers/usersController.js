e3b42f4f5d1c86408dd5af8c12e2a2cf85b3e743
arielgomez.87 <arielgomez.87@gmail.com>
2022-10-15 01:35:32 +0200

    creacion de models. crear usuario funcionando, falta login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,9 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { validationResult } = require('express-validator');
 const bcrypt = require("bcryptjs");
-
-const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
-let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const User = require("../models/User");
 
 
 const usersController = {
@@ -19,7 +17,7 @@ const usersController = {
 		res.render('register')
 	},
 	
-	// Create -  Method to store
+	// Create -  Method to create
 	processRegisterNewUser: (req, res) => {
 
 		let resultValidation = validationResult(req);
@@ -29,32 +27,43 @@ const usersController = {
 				errors:resultValidation.mapped(),
 				oldData:req.body
 			});
-		} else {
-		let userToRegister = req.body;
-		delete userToRegister.password_confirm;
-		let newUser={
-			id: users[users.length - 1].id + 1,
-			...userToRegister,
-			password: bcrypt.hashSync(userToRegister.password, 10),
-			confirm_password: bcrypt.hashSync(userToRegister.password, 10)
+		} 
+		let userInDataBase = User.findByField("user_name", req.body.user_name);
+
+		if(userInDataBase) {
+			return res.render("register", {
+				errors: {
+					user_name: {
+						msg: "Este usuario ya esta creado"
+					}
+				},
+				oldData : req.body
+			});
 		}
-		users.push(newUser)
-		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+		else {
+		let newUser={
+			...req.body,
+			password: bcrypt.hashSync(req.body.password, 10),
+			confirm_password: bcrypt.hashSync(req.body.confirm_password, 10),
+		}
+		let userCreated = User.create(newUser);
 		res.redirect('/users/login/');
 		}},
 
 	login:(req,res)=>{
 		res.render('login')
 	},
+
 	loginProcess: (req, res)=>{
 		let resultValidationLogin = validationResult(req);
 		
 		if (resultValidationLogin.errors.length > 0){
 			return res.render("login", {
-				errors:resultValidationLogin.mapped(),
-				oldData:req.body
+				errors:resultValidationLogin.mapped()
 			});
-
+		}
+		
+	},
 		/* console.log("user" + req.body.password);
 
 		const userToLogin = users.find(oneUser => oneUser.email === req.body.email);
@@ -76,7 +85,7 @@ const usersController = {
 		}*/
 		//return res.redirect('/');
 
-	}},
+	
 
 
 };
