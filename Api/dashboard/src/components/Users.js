8edea8c8345a00fa45fd38887646c60d5c07758f
nlomanto <nicolomanto@gmail.com>
2022-12-12 00:51:06 -0300

    Api Res Front Design

---
+++ Api/dashboard/src/components/Users.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from "react";
+
+
+  function Users({props}){
+  {/* xq no funciona el products.data.data? */}
+    console.log(props)
+
+   
+     return (
+    <>
+
+<div className="row mx-auto">
+<div className="col-lg-12 text-center card-header">
+<h1 className="m-2">Products in DB</h1>
+</div>
+
+{props.users.length > 0 && 
+            props.users.map(user => 
+            
+            
+            (
+                
+              <>
+              <div class="card shadow" style={{width: "18rem", margin: "1rem",maxHeight: "20rem"}}>
+                  <div class="card-body">
+                    <h5 class="card-title">{user.name}</h5>
+                    <p class="card-text"> <strong>Full Name: </strong>{user.fullName}</p>
+                    <p class="card-text"> <strong>Email: </strong>{user.email}</p>
+                    <p class="card-text"> <strong>Phone: </strong>{user.phoneNumber}</p>
+                    <p class="card-text"> <strong>Province: </strong>{user.Province.name}</p>
+                    <p class="card-text"> <strong>Address: </strong>{user.address}</p> 
+                    <p class="card-text"> <strong>Rol: </strong>{
+                      user.rolId != 2 ? "Usuario" : "Admin"
+                    }</p> 
+                  </div>
+                </div> 
+              </>
+  ))}
+
+
+
+          
+      
+
+
+    </div>
+    </>
+    
+      )
+}
+
+export default Users;
