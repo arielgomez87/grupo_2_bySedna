03f91cc11e5cdd32bd615b64321d3d536a2a0228
arielgomez.87 <arielgomez.87@gmail.com>
2022-12-12 05:28:09 +0000

    validaciones funcionando

--- bySedna/middlewares/validationsMiddlewareProduct.js
+++ bySedna/middlewares/validationsMiddlewareProduct.js
@@ -2,11 +2,11 @@ const { body } = require("express-validator");
 const path = require('path');
 
 const validationsProduct = [//validacion producto
- body("name").isLength({min:5}).withMessage("Completa el nombre del producto"),
- body("price").isLength({min:4}).withMessage("Completa el valor del producto"),
- body("discount").isLength({min:1}).withMessage("Completa el descuento del producto"),
- body("sizes").notEmpty().withMessage("Elige el talle o los talles del producto"),
- body("description").isLength({min:10}).withMessage("Completa una descripcion del producto"),
+ body("name").isLength({min:5}).withMessage("Completa el nombre del producto con minimo 5 caracteres"),
+ body("price").isLength({min:4}).withMessage("Completa el valor del producto con minimo 4 digitos"),
+ body("discount").isLength({min:1}).withMessage("Completa el descuento del producto con minimo 1 digito"),
+ body("sizes").notEmpty().withMessage("Elige un talle o mas del producto"),
+ body("description").isLength({min:10}).withMessage("Completa una descripcion del producto con minimo 10 caracteres"),
  body("productImage").custom((value, {req}) => {
      let file = req.file;
      let acceptedExtensions = [".jpg", ".jpeg",".png", "gif"];
---
+++ bySedna/public/js/js-frontProduct.js
@@ -0,0 +1,103 @@
+window.addEventListener("load", function(){
+
+
+    const nameField = document.querySelector("input.name");
+    const priceField = document.querySelector("input.price");
+    const discountField = document.querySelector("input.discount");
+    const sizesField = document.querySelector("select.sizes");
+    const descriptionField = document.querySelector("textarea.description");
+    const productImageField = document.querySelector("input.productImage");
+    
+    
+    const validacionCampoNombre = (e) => {
+            const campo = e.target;
+            const valorDelCampo = e.target.value;
+            if (valorDelCampo.trim().length < 5){
+                campo.classList.add("invalid");
+                campo.nextElementSibling.classList.add("error");
+                campo.nextElementSibling.innerText = "Completa el nombre del producto con minimo 5 caracteres";
+            } else{
+                campo.classList.remove("invalid");
+                campo.nextElementSibling.classList.remove("error");
+                campo.nextElementSibling.innerText = "";
+            }
+    };
+    
+    const validacionCampoPrecio = (e) => {
+        const campo = e.target;
+        const valorDelCampo = e.target.value;
+        if (valorDelCampo.trim().length < 4){
+            campo.classList.add("invalid");
+            campo.nextElementSibling.classList.add("error");
+            campo.nextElementSibling.innerText = "Completa el valor del producto con minimo 4 digitos";
+        } else{
+            campo.classList.remove("invalid");
+            campo.nextElementSibling.classList.remove("error");
+            campo.nextElementSibling.innerText = "";
+        }
+};
+
+    const validacionCampoDescuento = (e) => {
+        const campo = e.target;
+        const valorDelCampo = e.target.value;
+        if (valorDelCampo.trim().length < 1){
+            campo.classList.add("invalid");
+            campo.nextElementSibling.classList.add("error");
+            campo.nextElementSibling.innerText = "Completa el descuento del producto con minimo 1 digito";
+        } else{
+            campo.classList.remove("invalid");
+            campo.nextElementSibling.classList.remove("error");
+            campo.nextElementSibling.innerText = "";
+    }
+}
+
+    const validacionCampoTalle = (e) => {
+        const campo = e.target;
+        const valorDelCampo = e.target.value;
+        if (valorDelCampo == ""){
+            campo.classList.add("invalid");
+            campo.nextElementSibling.classList.add("error");
+            campo.nextElementSibling.innerText = "Elige un talle o mas del producto";
+        } else{
+            campo.classList.remove("invalid");
+            campo.nextElementSibling.classList.remove("error");
+            campo.nextElementSibling.innerText = "";
+        }
+    };
+    
+    const validacionCampoImagen= (e)=> {
+    const campo = e.target;
+    const fileExtension = e.target.files[0].name.split(".").pop().toLowerCase();
+    const validExtension = ["jpg", "jpeg", "png", "gif"];
+    if (!validExtension.includes(fileExtension)){
+        campo.classList.add("invalid");
+        campo.nextElementSibling.classList.add("error");
+        campo.nextElementSibling.innerText = `Las extensiones de archivos permitidas son:  ${validExtension.join(", ")}`;
+    } else{
+        campo.classList.remove("invalid");
+        campo.nextElementSibling.classList.remove("error");
+        campo.nextElementSibling.innerText = "";
+    }
+    }
+    
+    const validacionCampoDescripcion = (e) => {
+        const campo = e.target;
+        const valorDelCampo = e.target.value;
+        if (valorDelCampo.trim().length < 10){
+            campo.classList.add("invalid");
+            campo.nextElementSibling.classList.add("error");
+            campo.nextElementSibling.innerText = "Completa una descripcion del producto con minimo 10 caracteres";
+        } else{
+            campo.classList.remove("invalid");
+            campo.nextElementSibling.classList.remove("error");
+            campo.nextElementSibling.innerText = "";
+        }
+    };
+    
+    nameField.addEventListener("blur", validacionCampoNombre);
+    priceField.addEventListener("blur", validacionCampoPrecio);
+    discountField.addEventListener("blur", validacionCampoDescuento);
+    sizesField.addEventListener("blur", validacionCampoTalle);
+    productImageField.addEventListener("change", validacionCampoImagen);
+    descriptionField.addEventListener("blur", validacionCampoDescripcion);
+    })
